50868929e4228815968a6f800f5e4cd4991894c6
Nicholas Gust <nicholasgust@gmail.com>
2016-01-30 11:29:09 -0800

    made demo database, called .demo

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,17 @@
-# def convert_to_yml(yml_file)
-#   YAML.load_file(File.absolute_path(yml_file))
-# end
+def convert_to_yml(yml_file)
+  YAML.load_file(File.absolute_path(yml_file))
+end
 
 
 
-# user_yml = convert_to_yml('db/data/users.yml')
+user_yml = convert_to_yml('db/data/users.yml')
 
-# user_yml.each_pair do |name, info|
-#   User.create!(
-#     username: name,
-#     email: info["email"],
-#     password: info["password"],
-#     img_path: info["img_path"],
-#     bio: info["bio"]
-#   )
-# end
+user_yml.each_pair do |name, info|
+  User.create!(
+    username: name,
+    email: info["email"],
+    password: info["password"],
+    img_path: info["img_path"],
+    bio: info["bio"]
+  )
+end
