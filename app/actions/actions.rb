f74b48785966b95b249dccff5939598df19c7540
jdmor <jona.morrissey@gmail.com>
2016-01-30 18:35:42 +0000

    Move dev routes to static pages to main actions.rb

--- app/actions/actions.rb
+++ app/actions/actions.rb
@@ -55,12 +55,52 @@ end
 
 
 
-# # Development
+# Development
 
-# get '/dev/match' do
-#   erb :'/dev/match/index'
-# end
+get '/dev/match' do
+  erb :'/dev/match/index'
+end
 
-# get '/dev/match/pending' do
-#   erb :'/dev/match/pending'
-# end
+get '/dev/match/requests' do
+  erb :'/dev/match/requests'
+end
+
+get '/dev/match/pending' do
+  erb :'/dev/match/pending'
+end
+
+get '/dev/match/record' do
+  erb :'/dev/match/record'
+end
+
+get '/dev/match/select_winner' do
+  erb :'/dev/match/select_winner'
+end
+
+get '/dev/match/match_saved' do
+  erb :'/dev/match/match_saved'
+end
+
+get '/dev/match/challenge_issued' do
+  erb :'/dev/match/challenge_issued'
+end
+
+get '/dev/match/accepted-notification' do
+  erb :'/dev/match/accepted_notification'
+end
+
+get '/dev/match/add-player' do
+  erb :'/dev/match/add_player'
+end
+
+get '/dev/match/add-players' do
+  erb :'/dev/match/add_players'
+end
+
+get '/dev/rules' do
+  erb :'/dev/rules/index'
+end
+
+get '/dev/leaderboard' do
+  erb :'/dev/leaderboard/index'
+end
