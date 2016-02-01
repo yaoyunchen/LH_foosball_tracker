b3eddc32b443089a96d598ce46d8175f1e078dd6
Andy Yao <a_n_d_y_y_a_o@hotmail.com>
2016-02-01 07:26:07 +0000

    Fixed winner selection routing

--- app/actions/user_actions.rb
+++ app/actions/user_actions.rb
@@ -254,27 +254,23 @@ end
 put '/user/pending_matches' do
   redirect '/users/login' unless current_user
   @match = Match.find_by(id: params[:match_id].to_i)
-  p params
+  
   if params[:status] == "cancelled"
     @match.match_cancelled
     redirect '/user/pending_matches'
   else
-    redirect '/user/pending_matches/choose_winner'
+    redirect "/user/pending_matches/#{params[:match_id]}/choose_winner"
   end
 end
 
-get '/user/pending_matches/choose_winner' do
+get '/user/pending_matches/:match_id/choose_winner' do
   redirect '/users/login' unless current_user
-
-  #match = Match.find params[:match_id]
-  match = Match.find(31)
-
+  
+  match = Match.find params[:match_id]
   team_left = []
   team_right = []
 
   if match.category == "singles"
-
-
     left_player = MatchInvite.find_by(match_id: match.id, user_id: current_user.id)
     team_left << {
       username: User.find_by(id: left_player.user_id).username,
@@ -288,44 +284,40 @@ get '/user/pending_matches/choose_winner' do
       side: right_player[0].side
     }
   else
+    invites = MatchInvite.where(match_id: match.id)
 
-    left_players = ""
-
-
-
-
-    right_players = ""
-
+    invites.each do |invite|
+      if invite.side == "1"
+        team_left << {
+          username: User.find_by(id: invite.user_id).username,
+          img_path: User.find_by(id: invite.user_id).img_path,
+          side: invite.side
+        }
+      else
+        team_right << {
+          username: User.find_by(id: invite.user_id).username,
+          img_path: User.find_by(id: invite.user_id).img_path,
+          side: invite.side
+        }
+      end
+    end
   end
 
   team_left
   team_right
-  # team_left = MatchResult.where(match_id: match.id ,user_id: current_user.id)
-  # players_left = []
-  # team_left.each do |member|
-  #   player = User.find_by(id: member.user_id)
-  #   players_left << {username: player.username, img_path: player.img_path, team: member.team}
-  # end
 
-  # team_right = MatchResult.where.not(team: team_left[0].team).where(match_id: match.id)
-  # players_right = []
-  # team_right.each do |member|
-  #   player = User.find_by(id: member.user_id)
-  #   players_right << {username: player.username, img_path: player.img_path, team: member.team}
-  # end
   @result = []
   @result << {
     left: team_left, 
     right: team_right, 
     id: match.id
   }
-
   erb :'/user/pending_matches/choose_winner'
 end
 
 
 put '/user/pending_matches/choose_winner' do
-  #@match = Match.find params[:match_id]
-  #@match.match_over(params[:team])
-  #redirect '/'
+  @match = Match.find params[:match_id]
+  @match.match_over(params[:team])
+  redirect '/'
 end
