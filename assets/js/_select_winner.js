f92cd9cdf55b9919dd1244bf2fb69ba82b22b363
jdmor <jona.morrissey@gmail.com>
2016-02-01 15:34:02 +0000

    Style record match page

--- assets/js/_select_winner.js
+++ assets/js/_select_winner.js
@@ -1,6 +1,8 @@
 $('.js-select-winner').click(function() {
-  var selected_team = $(this).attr('data-team');
+  var selected_team = $(this).attr('data-team')
+  $(this).addClass('winner-is-selected');
+  $(this).siblings('.js-select-winner').removeClass('winner-is-selected');
   $('#ChooseWinner').find('input[name="team"]').attr('value', selected_team);
 
-  $('#ChooseWinner').find('.button').removeClass('is-disabled');
+  $('#ChooseWinner').find('.button').removeClass('is-disabled').addClass('is-success');
 });
--- public/javascript/application.js
+++ public/javascript/application.js
@@ -202,9 +202,11 @@ $('.js-record-match').click(function() {
   });
 });
 $('.js-select-winner').click(function() {
-  var selected_team = $(this).attr('data-team');
+  var selected_team = $(this).attr('data-team')
+  $(this).addClass('winner-is-selected');
+  $(this).siblings('.js-select-winner').removeClass('winner-is-selected');
   $('#ChooseWinner').find('input[name="team"]').attr('value', selected_team);
 
-  $('#ChooseWinner').find('.button').removeClass('is-disabled');
+  $('#ChooseWinner').find('.button').removeClass('is-disabled').addClass('is-success');
 });
 });
